d25d071c36cbf710f147368df3c2f5931273fbcb
Xenon <167872462+XenonBlack999@users.noreply.github.com>
2025-05-14 15:54:04 +0700

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,8 @@
+text = "python"
+print(f"+{text}+{text}+{text}+")
+for i in text:
+    print(i+' '*len(text)+i+' '*len(text)+i+' '*len(text)+i)
+print(f"+{text}+{text}+{text}+")
+for i in text:
+    print(i+' '*len(text)+i+' '*len(text)+i+' '*len(text)+i)
+print(f"+{text}+{text}+{text}+")
